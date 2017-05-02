105b41d96a540321c73144e4d2109aca848069e9
annanormark <anna.nor94@gmail.com>
2017-05-02 11:10:45 +0200

    removed comments

--- FlappyFabian/app/src/main/java/com/example/flappyfabian/Game.java
+++ FlappyFabian/app/src/main/java/com/example/flappyfabian/Game.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Game extends AppCompatActivity {
-    //Hallåååååååå
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,4 +16,3 @@ public class Game extends AppCompatActivity {
     }
 
 }
-//HAR JU KMOMENTERAT SKIT!!
